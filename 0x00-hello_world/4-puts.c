f5a4a7ac894575fdc5da8b1e6db9ba83f2f5dff2
Arsany199 <arsanykamal30@gmail.com>
2023-08-10 11:42:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * int main() - return 0
- *
- * return: always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
