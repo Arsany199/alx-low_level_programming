e475705a8f486ad2ff697b6778763fbc7201069a
Arsany199 <arsanykamal30@gmail.com>
2023-08-28 08:17:28 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+	int  j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+					{
+						return (s);
+
+					}
+		}
+		s ++;
+	}
+	return ('\0');
+}
