ee8702e4f103b9bc55357ea2b9353989605357cb
Arsany199 <arsanykamal30@gmail.com>
2023-09-01 06:31:30 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print number of the argument
+ * @argc: the number
+ * @argv: the array of commands
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+}
