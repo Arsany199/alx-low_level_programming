cf796a10c1ebd66216293ba978526cd3df48d9cb
Arsany199 <arsanykamal30@gmail.com>
2023-09-11 08:13:09 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,31 +3,42 @@
 #include <string.h>
 #include "dog.h"
 
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog_ptr;
+	dog_t *new_dog_ptr;
 
-    new_dog_ptr = malloc(sizeof(dog_t));
-    if (new_dog_ptr == NULL) {
-        return NULL;
-    }
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+	{
+		return (NULL);
+	}
 
-    new_dog_ptr->name = malloc(strlen(name) + 1);
-    if (new_dog_ptr->name == NULL) {
-        free(new_dog_ptr);
-        return NULL;
-    }
-    strcpy(new_dog_ptr->name, name);
+	new_dog_ptr->name = malloc(strlen(name) + 1);
+	if (new_dog_ptr->name == NULL)
+	{
+		free(new_dog_ptr);
+		return (NULL);
+	}
+	strcpy(new_dog_ptr->name, name);
 
-    new_dog_ptr->owner = malloc(strlen(owner) + 1);
-    if (new_dog_ptr->owner == NULL) {
-        free(new_dog_ptr->name);
-        free(new_dog_ptr);
-        return NULL;
-    }
-    strcpy(new_dog_ptr->owner, owner);
+	new_dog_ptr->owner = malloc(strlen(owner) + 1);
+	if (new_dog_ptr->owner == NULL)
+	{
+		free(new_dog_ptr->name);
+		free(new_dog_ptr);
+		return (NULL);
+	}
+	strcpy(new_dog_ptr->owner, owner);
 
-    new_dog_ptr->age = age;
+	new_dog_ptr->age = age;
 
-    return new_dog_ptr;
+	return (new_dog_ptr);
 }
