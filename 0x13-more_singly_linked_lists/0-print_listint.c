1052a5e465d59396bcb5a864d317be86228cd029
Arsany199 <arsanykamal30@gmail.com>
2023-09-25 03:32:08 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,11 +14,9 @@ size_t print_listint(const listint_t *h)
 	{
 		printf("%d\n", h->n);
 
-		if (h->next)
-			printf(" ");
 		h = h->next;
 		size++;
 	}
-	printf("\n");
 	return (size);
+	printf("\n");
 }
