138850d136d09a68ac110e39c21a1491895fcbf4
Arsany199 <arsanykamal30@gmail.com>
2023-09-25 06:37:48 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int my_data;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	my_data = temp->n;
+	*head = temp->next;
+	free(temp);
+	return (my_data);
+}
