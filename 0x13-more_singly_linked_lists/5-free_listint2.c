7a4785a59c3dc92813346a45c83d392bcc4c68d5
Arsany199 <arsanykamal30@gmail.com>
2023-09-25 15:57:18 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - frees a linked list
+ * @head: double pointer on the first node
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
