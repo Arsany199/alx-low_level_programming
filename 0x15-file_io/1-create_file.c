716776335e0ecba6fa99d21daa4be554a43dd999
Arsany199 <arsanykamal30@gmail.com>
2023-10-02 08:36:13 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,18 +10,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f, w, len;
+	int f, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		len = 0;
-		while (text_content[len])
-		{
+		for(len = 0; text_content[len];)
 			len++;
-		}
 	}
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(f, text_content, len);
