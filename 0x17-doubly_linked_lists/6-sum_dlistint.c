cd880d778259b8337aa4ff3cf604096e2e6adddc
Arsany199 <arsanykamal30@gmail.com>
2023-11-09 06:09:00 -0800

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all elements in the list
+ * @head: the head of the list
+ * Return: the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	{
+
+	while (head->prev != NULL)
+	{
+		head = head->prev;
+	}
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	}
+	
+	return (sum);
+}
